1e24e504f462538e00eb2233f5e3632daa4b7ac8
Josiah Johnson <josiahdj@gmail.com>
2016-01-16 17:12:46 -0800

    initial commit

---
+++ App.fs
@@ -0,0 +1,25 @@
+﻿module SuaveMusicStore.App
+
+open Suave
+open Suave.RequestErrors
+open Suave.Successful
+open Suave.Filters
+open Suave.Operators
+
+
+let browse =
+    request (fun r ->
+        match r.queryParam "genre" with
+        | Choice1Of2 genre -> OK (sprintf "Genre: %s" genre)
+        | Choice2Of2 msg -> BAD_REQUEST msg)
+
+
+let webPart = 
+    choose [
+        path "/" >=> (OK "Home")
+        path "/store" >=> (OK "Store")
+        path "/store/browse" >=> browse
+        pathScan "/store/details/%d" (fun id -> OK (sprintf "Details: %d" id))
+    ]
+
+startWebServer defaultConfig webPart
---
+++ View.fs
@@ -0,0 +1,3 @@
+﻿module SuaveMusicStore.View
+
+open Suave
